fb45bba9074fa1e2393fdaa14ebacb5aec0c4468
Iker <barry_>
2025-02-11 00:03:20 +0100

    Rename callback to finalCallback

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,7 +6,7 @@ const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_b0tpHM55u",
   client_id: "4fbadbb2qqj15u0vf5dmauudbj",
   // El redirect_uri debe coincidir con el que configuraste en el callback:
-  redirect_uri: "https://dashboard.total-remote-control.com/api/auth/callback",
+  redirect_uri: "https://dashboard.total-remote-control.com/api/auth/finalCallback",
   response_type: "code",
   scope: "email openid profile",
 };
