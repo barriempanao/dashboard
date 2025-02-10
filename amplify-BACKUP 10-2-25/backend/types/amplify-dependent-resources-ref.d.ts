16fd2007f8c3241ddf296ab0605802efb4b52cf1
Iker <barry_>
2025-02-10 19:11:28 +0100

    Sincronización del backend con AppID d2ma6sromsjebz

---
+++ amplify-BACKUP 10-2-25/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dashboardf5d878dd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s35971a595": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
