2f97a24de0601f8256caf401fc79c3198327755e
Iker <barry_>
2025-02-11 18:42:31 +0100

    Arreglar .

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 🚀 Deshabilita ESLint en el build de producción
   },
+  env: {
+    COGNITO_DOMAIN: process.env.COGNITO_DOMAIN,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_REDIRECT_URI: process.env.COGNITO_REDIRECT_URI,
+  },
 };
 
 module.exports = nextConfig;
