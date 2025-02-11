4360908ae0af609b996d03182fc5a1b6e2a1fe0d
Iker <barry_>
2025-02-11 22:53:44 +0100

    CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,11 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className="p-10 bg-red-500 text-white">
-      Si ves esto en rojo, Tailwind está funcionando.
-    </div>
+    <Layout>
+      <div>
+        <h1>Bienvenido a la aplicación</h1>
+        <p>Este es el inicio de tu dashboard.</p>
+      </div>
+    </Layout>
   );
 }
