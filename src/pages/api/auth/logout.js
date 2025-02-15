e8a606b3b654f68bc528352bd8001998dc110049
Iker <barry_>
2025-02-15 22:16:18 +0100

    Arreglando layout y errores de acceso

--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -1,6 +1,14 @@
 // pages/api/auth/logout.js
 export default function handler(req, res) {
-  // Asegúrate de que las opciones coincidan con las usadas para establecer la cookie.
-  res.setHeader('Set-Cookie', `authToken=; Path=/; HttpOnly; Secure; SameSite=Strict; expires=Thu, 01 Jan 1970 00:00:00 GMT`);
-  res.status(200).json({ message: 'Logout successful' });
+  // Borra la cookie usando las mismas opciones con las que se estableció
+  res.setHeader(
+    'Set-Cookie',
+    `authToken=; Path=/; HttpOnly; Secure; SameSite=Strict; expires=Thu, 01 Jan 1970 00:00:00 GMT`
+  );
+
+  // Construye la URL de logout de Cognito
+  const cognitoLogoutURL = `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}&logout_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI)}`;
+
+  // Redirige a Cognito para cerrar sesión también allí
+  res.redirect(cognitoLogoutURL);
 }
