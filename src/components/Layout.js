ca8962939ed97715ea4d4581154513ecacaac5be
Iker <barry_>
2025-02-15 21:40:06 +0100

    Arreglando layout. Datos vas bien

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,27 @@
 // components/Layout.js
 
+
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Sidebar />
+      <main className="main-content">
+        <Header />
+        <div className="content">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+
+
+
+/*
 import Header from './Header';
 import Sidebar from './Sidebar';
 
@@ -14,34 +36,4 @@ export default function Layout({ children }) {
     </div>
   );
 }
-
-/*
-import Header from './Header';
-import Sidebar from './Sidebar';
-
-export default function Layout({ children }) {
-  return (
-    <div style={layoutStyle}>
-      <Sidebar />
-      <div style={mainContentStyle}>
-        <Header />
-        <div style={contentStyle}>
-          {children}
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const layoutStyle = {
-  display: 'flex',
-};
-
-const mainContentStyle = {
-  flex: 1,
-};
-
-const contentStyle = {
-  padding: '20px',
-};
 */
