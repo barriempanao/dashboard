5b4f27eeed4a789457de7bb02d2c31168fc5b554
Iker <barry_>
2025-02-16 11:33:51 +0100

    Implementar licencias

--- src/lib/db.js
+++ src/lib/db.js
@@ -25,3 +25,11 @@ export async function getUserByEmail(email) {
   console.log("Usuario encontrado:", rows[0]); // Muestra el usuario encontrado
   return rows.length > 0 ? rows[0] : null;
 }
+
+export async function getLicensesByUserId(user_id) {
+  const pool = await getPool();
+  // Consulta para obtener todas las licencias asociadas al usuario
+  const [rows] = await pool.query('SELECT * FROM licenses WHERE user_id = ?', [user_id]);
+  console.log("Licencias encontradas para el user_id", user_id, ":", rows);
+  return rows;
+}
