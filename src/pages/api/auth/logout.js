2d3a4f6f42445b68c5c656a6603d763ee8919b81
Iker <barry_>
2025-02-16 03:52:16 +0100

    errores de acceso

--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -7,8 +7,10 @@ export default function handler(req, res) {
   );
 
   // Construye la URL de logout de Cognito
-  //const cognitoLogoutURL = `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}&logout_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI)}`;
-    const cognitoLogoutURL = `https://total-remote-control.com`;
+  const cognitoLogoutURL = `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}&logout_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI)}`;
+    
+    const cognitoLogoutURL = `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}&logout_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_COGNITO_REDIRECT_LOGOUT_URI)}`;
+
 
   // Redirige a Cognito para cerrar sesión también allí
   res.redirect(cognitoLogoutURL);
